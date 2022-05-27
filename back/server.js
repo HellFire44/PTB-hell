a118ddb7e4e58deecb7de4351fe986463b5b1d13
Jivadio <malomartin@protonmail.com>
2022-05-27 22:59:22 +0200

    Update server.js

--- back/server.js
+++ back/server.js
@@ -47,6 +47,7 @@ server.on('listening', () => {
 	const address = server.address();
 	const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
 	console.log('Listening on ' + bind);
+    console.log('Link:  http://localhost:' + port);
 });
 
 server.listen(port);
